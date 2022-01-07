ea58110fd14f8761690b5292dcf9124d27cc62ab
yom <jaxad@ya.ru>
2022-01-07 22:03:28 +0300

    +follow/unfollow

--- jitter3_10/jitter/apps/jitterprofile/views.py
+++ jitter3_10/jitter/apps/jitterprofile/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def jitterprofile(request, username):
@@ -10,3 +11,19 @@ def jitterprofile(request, username):
     }
 
     return render(request, 'jitterprofile/jitterprofile.html', context)
+
+@login_required
+def follow_jitter(request, username):
+    user = get_object_or_404(User, username=username)
+
+    request.user.jitterprofile.follow.add(user.jitterprofile)
+
+    return redirect('jitterprofile', username = username)
+
+@login_required
+def unfollow_jitter(request, username):
+    user = get_object_or_404(User, username=username)
+
+    request.user.jitterprofile.follow.remove(user.jitterprofile)
+
+    return redirect('jitterprofile', username = username)
